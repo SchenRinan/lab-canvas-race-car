2c40336c8f868e6093e2622faf6efa7536e0334e
Rinan <jandrewschnell@gmail.com>
2023-04-30 13:13:54 +0200

    crash edge done

--- js/index.js
+++ js/index.js
@@ -19,20 +19,22 @@ roadImg.onload = function() {
 //Move Road
 const moveRoad = {
   img: roadImg,
-  y: 0,
-  speed: -10,
+  roadY: 0,
+  roadSpeed: -10,
+  roadFrames: 0,
 
   move: function() {
-    this.y -= this.speed;
-    this.y %= canvas.height;
+    this.roadFrames++;
+    this.roadY -= this.roadSpeed;
+    this.roadY %= canvas.height;
   },
 
   draw: function() {
-    ctx.drawImage(this.img, 0, this.y, canvas.width, canvas.height);
-    if (this.speed < 0) {
-      ctx.drawImage(this.img, 0, this.y - canvas.height, canvas.width, canvas.height);
+    ctx.drawImage(this.img, 0, this.roadY, canvas.width, canvas.height);
+    if (this.roadSpeed < 0) {
+      ctx.drawImage(this.img, 0, this.roadY - canvas.height, canvas.width, canvas.height);
     } else {
-      ctx.drawImage(this.img, 0, this.y + this.img.height, canvas.width, canvas.height);
+      ctx.drawImage(this.img, 0, this.roadY + this.img.height, canvas.width, canvas.height);
     }
   },
 };
@@ -45,33 +47,65 @@ carImage.onload = function() {
   ctx.drawImage(carImage, 200, 500, 100, 200);
 };
 
-
 const moveCar = {
   img: carImage,
-  speed: 0,
+  carSpeed: 0,
   x: 200,
 
   draw: function() {
-    if(this.speed === 0)
-    {this.x += this.speed;}
-    else if(this.speed <1){
-      this.x -= 5;}
-    else if(this.speed>1){
-      this.x += 5;}
+    if(this.carSpeed === 0)
+    {this.x += this.carSpeed;}
+    else if(this.carSpeed <1){
+      this.x -= 8;}
+    else if(this.carSpeed>1){
+      this.x += 8;}
 
-      if(this.x<10){this.x=10}
-      if(this.x>390)(this.x=390)
-    console.log(this.speed)
+    if(this.x<0){this.x=0}
+    if(this.x>400)(this.x=400)
     ctx.drawImage(this.img, this.x, 500, 100, 200);
   },
 };
 
+//Upload Obstacle
+const obstacleArray = [];
+const generateObstacle = {
+  obsY: 0,
+  obsSpeed: -10,
+  xInitial: Math.floor(Math.random() * 350),
+  xSize: 100 + Math.floor(Math.random() * 150),
+
+  move: function() {
+    this.obsY -= this.obsSpeed;
+  },
+  draw: function() {
+    ctx.fillStyle = 'red'; ctx.fillRect(this.xInitial, this.obsY, this.xSize, 30);
+  },
+}
+
+function crashed(obstacle){
+  // if(obstacle[0].obsY===470 && moveCar.x===){return true}
+  return moveCar.x === 0 || moveCar.x === 400;
+  // return !(this.bottom() < obstacle.top() || this.top() > obstacle.bottom() || this.right() < obstacle.left() || this.left() > obstacle.right());
+}
+
+
 function updateCanvas() {
+  if(moveRoad.roadFrames % 120 === 0){
+    obstacleArray.unshift(generateObstacle);
+    obstacleArray[0].obsY = 0;
+    obstacleArray[0].xInitial = Math.floor(Math.random() * 350);
+    obstacleArray[0].xSize = 100 + Math.floor(Math.random() * 150);
+  }
   moveRoad.move();
+  obstacleArray[0].move()
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   moveRoad.draw();
   moveCar.draw();
-  requestAnimationFrame(updateCanvas);
+  obstacleArray[0].draw();
+  let animation = requestAnimationFrame(updateCanvas);
+  //crash conditions
+  if(crashed(obstacleArray))
+  {cancelAnimationFrame(animation)}
 }
 
 
@@ -85,14 +119,14 @@ document.addEventListener('keydown', (e) => {
   switch (e.keyCode) {
     case 37: // left arrow
     // console.log(speed)
-      moveCar.speed -= 5;
+      moveCar.carSpeed -= 8;
       break;
     case 39: // right arrow
-      moveCar.speed += 5;
+      moveCar.carSpeed += 8;
       break;
   }
 });
 
 document.addEventListener('keyup', (e) => {
-  moveCar.speed = 0;
+  moveCar.carSpeed = 0;
 });
